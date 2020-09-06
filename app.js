638bba565e20f35943134fd243cff96f6ab959e2
Viplow <viplow.291@gmail.com>
2020-09-06 12:10:05 +0530

    Added Hashing

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const bodyParser = require("body-parser");
 const ejs = require('ejs');
 const app=express();
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
+
 app.set('view engine', 'ejs');
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true,useUnifiedTopology: true });
 const userSchema=new mongoose.Schema({
@@ -13,7 +14,6 @@ const userSchema=new mongoose.Schema({
   password:String
 });
 
-userSchema.plugin(encrypt,{secret : process.env.SECRET,encryptedFields:['password']});
 const User =new mongoose.model("User",userSchema);
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -30,7 +30,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
   const newUser=new User({
     email:req.body.username,
-    password:req.body.password
+    password:md5(req.body.password)
   });
   newUser.save(function(err){
     if(err){
@@ -43,7 +43,7 @@ app.post("/register",function(req,res){
 });
 app.post("/login",function(req,res){
   const username=req.body.username;
-  const password=req.body.password;
+  const password=md5(req.body.password);
   User.findOne({email:username},function(err,found){
     if(err){
       console.log(err);
